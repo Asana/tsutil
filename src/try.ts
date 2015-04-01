99bcb04dd836aeaacb1dfb16fdd600ec1f62dd99
Daniel Phan <daniel@asana.com>
2015-04-01 13:41:48 -0700

    Add E extends Error type parameter to Try

--- src/try.ts
+++ src/try.ts
@@ -3,8 +3,8 @@ import Optional = require("./optional");
 /**
  * The Try class is used to represent an error xor a value
  */
-class Try<T> {
-    private _err: Error;
+class Try<E extends Error, T> {
+    private _err: E;
     private _value: T;
 
     /**
@@ -12,9 +12,9 @@ class Try<T> {
      * @param tries The list of tries to flatten
      * @returns {Try<T[]>}
      */
-    static all<T>(tries: Try<T>[]): Try<T[]> {
-        return Try.attempt<T[]>(() => {
-            return tries.reduce((acc: T[], elem: Try<T>) => {
+    static all<E extends Error, T>(tries: Try<E, T>[]): Try<E, T[]> {
+        return Try.attempt<E, T[]>(() => {
+            return tries.reduce((acc: T[], elem: Try<E, T>) => {
                 acc.push(elem.getOrThrow());
                 return acc;
             }, []);
@@ -29,13 +29,13 @@ class Try<T> {
      *   caught and stored.
      * @returns {Try<T>}
      */
-    static attempt<T>(fn: () => T, filter?: (err: Error) => boolean): Try<T> {
+    static attempt<E extends Error, T>(fn: () => T, filter?: (err: E) => boolean): Try<E, T> {
         try {
             var value = fn();
-            return new Try<T>(value);
+            return new Try<E, T>(null, value);
         } catch (err) {
             if (!filter || filter(err)) {
-                return new Try<T>(null, err);
+                return new Try<E, T>(err, null);
             } else {
                 throw err;
             }
@@ -47,8 +47,8 @@ class Try<T> {
      * @param value The value for the Try
      * @returns {Try<T>}
      */
-    static success<T>(value: T): Try<T> {
-        return new Try<T>(value);
+    static success<E extends Error, T>(value: T): Try<E, T> {
+        return new Try<E, T>(null, value);
     }
 
     /**
@@ -56,8 +56,8 @@ class Try<T> {
      * @param err The error for the Try
      * @returns {Try<T>}
      */
-    static failure<T>(err: Error): Try<T> {
-        return new Try<T>(null, err);
+    static failure<E extends Error, T>(err: E): Try<E, T> {
+        return new Try<E, T>(err, null);
     }
 
     /**
@@ -65,7 +65,7 @@ class Try<T> {
      * @param value The value for the Try
      * @param err The optional error that will override the value
      */
-    constructor(value: T, err: Error = null) {
+    constructor(err: E, value: T) {
         this._value = value;
         this._err = err;
     }
@@ -74,7 +74,7 @@ class Try<T> {
      * The error of the try
      * @returns {Error}
      */
-    error(): Error {
+    error(): E {
         return this._err;
     }
 
@@ -99,11 +99,11 @@ class Try<T> {
      * @param filterer A filter function to check the value against
      * @returns {*}
      */
-    filter(filterer: (value: T) => boolean): Try<T> {
-        if (this.isFailure() || filterer(this._value)) {
+    filter(predicate: (value: T) => boolean): Try<Error, T> {
+        if (this.isFailure() || predicate(this._value)) {
             return this;
         }
-        return Try.failure<T>(new Error("Try.filter: No such element"));
+        return Try.failure<Error, T>(new Error("Try#filter(): No such element"));
     }
 
     /**
@@ -121,13 +121,13 @@ class Try<T> {
      * @param mapper A map function to create the new value
      * @returns {Try<U>}
      */
-    map<U>(mapper: (value: T) => U): Try<U> {
+    map<U>(mapper: (value: T) => U): Try<Error, U> {
         if (this.isSuccess()) {
-            return Try.attempt<U>(() => {
+            return Try.attempt<Error, U>(() => {
                 return mapper(this._value);
             });
         }
-        return (<Try<U>><{}>this);
+        return (<Try<E, U>><{}>this);
     }
 
     /**
@@ -135,11 +135,11 @@ class Try<T> {
      * @param mapper A map function that returns a try
      * @returns {Try<U>}
      */
-    flatMap<U>(mapper: (value: T) => Try<U>): Try<U> {
+    flatMap<F extends E, U>(mapper: (value: T) => Try<F, U>): Try<E, U> {
         if (this.isSuccess()) {
             return mapper(this._value);
         }
-        return (<Try<U>><{}>this);
+        return (<Try<E, U>><{}>this);
     }
 
     /**
@@ -157,7 +157,7 @@ class Try<T> {
      * @param other The fallback accessor
      * @returns {*}
      */
-    getOrElse<U extends T>(other: (err: Error) => U): T {
+    getOrElse<U extends T>(other: (err: E) => U): T {
         if (this.isSuccess()) {
             return this._value;
         }
@@ -169,11 +169,11 @@ class Try<T> {
      * @param other The fallback try accessor
      * @returns {*}
      */
-    orElse<U extends T>(other: () => Try<U>): Try<T> {
+    orElse<U extends T>(other: (err: E) => Try<E, U>): Try<Error, T> {
         if (this.isSuccess()) {
             return this;
         }
-        return other();
+        return other(this._err);
     }
 
     /**
@@ -193,7 +193,7 @@ class Try<T> {
      * @param onFailure The callback for when the try is failed
      * @returns {Try<U>}
      */
-    transform<U>(onSuccess: (value: T) => Try<U>, onFailure: (err: Error) => Try<U>): Try<U> {
+    transform<F extends Error, U>(onSuccess: (value: T) => Try<F, U>, onFailure: (err: Error) => Try<F, U>): Try<F, U> {
         if (this.isSuccess()) {
             return onSuccess(this._value);
         }
